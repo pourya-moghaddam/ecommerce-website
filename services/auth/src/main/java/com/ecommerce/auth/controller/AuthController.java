a042f5db219f7197c8c9db1d46b47d0fe2e80815
pourya <pou.moghaddam@gmail.com>
2025-08-22 17:45:25 +0330

    Set up Spring Boot app for auth service

---
+++ services/auth/src/main/java/com/ecommerce/auth/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.ecommerce.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Auth service is running!";
+    }
+
+}
